c8e374f9b6f1efd9aec11a194b698c3b332cc562
Thomas J Bradley <theman@thomasjbradley.ca>
2012-07-03 17:34:41 -0400

    Changed bootstrap process to create symlinks.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
-# Stolen from: https://github.com/mathiasbynens/dotfiles
+exclude=(".", "..", ".git", "bootstrap.sh", "README.md")
+PWD=`pwd`
 
-cd "$(dirname "$0")"
-git pull
-function doIt() {
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-}
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt
-else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
-  fi
-fi
-unset doIt
-source ~/.bash_profile
+for f in `ls -a`
+do
+  case "${exclude[@]}" in  *"$f"*) continue ;; esac
+  ln -fs $PWD/$f ~/$f
+done
