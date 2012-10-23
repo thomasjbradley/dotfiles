def92d05a88518651f113c0c1fc6ddd509b8dbb5
Thomas J Bradley <theman@thomasjbradley.ca>
2012-10-23 15:50:52 -0400

    Added some commonly used aliases to the bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,12 @@ do
   case "${exclude[@]}" in  *"$f"*) continue ;; esac
   ln -fsn $PWD/$f ~/$f
 done
+
+# Set up some commonly used aliases
+ln -s $HOME/Dropbox/ /db
+ln -s $HOME/Dropbox/www /db/www
+ln -s $HOME/Dropbox/twww /db/www/-tmp
+ln -s $HOME/Dropbox/dwww /db/www/-dev
+ln -s $HOME/Dropbox/vwww /db/www/-vwww
+ln -s $HOME/Dropbox/tjb-dev /db/tjb-dev
+ln -s /Applications/MAMP/htdocs /mamp
